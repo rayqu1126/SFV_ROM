6ab58fc345474dadf19e3b1ae9d515a86236d2dc
Ray Qu <rayqu1126@gmail.com>
2026-05-07 22:21:56 -0500

    Update SFVM2D_euler.m

--- SFVM2D_euler.m
+++ SFVM2D_euler.m
@@ -1,4 +1,4 @@
-%% SFVM and ROM for 1D Euler equations with 1D stochastic variables
+%% SFVM and ROM for 1D Euler equations with one stochastic variables
 clear
 close all
 addpath('func')
@@ -10,10 +10,20 @@ set(0, 'defaultaxesfontsize',24,'defaultaxeslinewidth',2,...
 set(0, 'DefaultFigurePosition',[0 0 600 400]);
 
 % Initialize mesh (Nx: physical Ny: stochastic)
-Nx = 128;
-Ny = 32;
+Nx = 512;
 
-xpts = linspace(0,1,Nx+1);
+% Ny_list = [4;8;16;32]
+% state_time_list = [];
+% state_step_list = [];
+% flux_time_list = [];
+% flux_step_list = [];
+% difference_list = [];
+% 
+% for r = 1:length(Ny_list)
+% Ny = Ny_list(r);
+
+% xpts = linspace(0,1,Nx+1);
+xpts = linspace(-5,5,Nx+1);
 ypts = linspace(0,1,Ny+1);
 
 x = 0.5*(xpts(1:Nx)+xpts(2:Nx+1));
@@ -23,26 +33,39 @@ y = 0.5*(ypts(1:Ny)+ypts(2:Ny+1));
 
 % Initialize solution array
 t0=0;
-tmax=0.2;
+
+% tmax = 0.2; % for Sod 
+tmax= 1.8; % for Shu-Osher
 tspan = [t0 tmax];
 
-u = zeros(Nx,Ny);
-rho = ones(Nx,Ny) * 0.125;
-p = ones(Nx,Ny) * 0.1;
+% IC for Sod shock tube 
+% u = zeros(Nx,Ny);
+% rho = ones(Nx,Ny) * 0.125;
+% p = ones(Nx,Ny) * 0.1;
+% rho(X < 0.475 + 0.05*Y) = 1; 
+% p(X < 0.475 + 0.05*Y) = 1;   
+% 
+% IC for Shu-Osher shock tube
+u = ones(Nx,Ny) * 2.629369;
+rho = ones(Nx,Ny) * 3.857143;
+p = ones(Nx,Ny) * 10.3333;
+
+u(X >= -4.1 + 0.2*Y) = 0;
+rho(X >= -4.1 + 0.2 * Y) = 1 + 0.2 * sin(5 * X(X >= -4.1 + 0.2 * Y));
+p(X >= -4.1 + 0.2*Y) = 1;   
 
 
-rho(X < 0.475 + 0.05*Y) = 1; 
-p(X < 0.475 + 0.05*Y) = 1;   
 gamma = 1.4;
 E = p / (gamma - 1) + 0.5 * rho .* u.^2;
 
 ics = [rho; rho .* u; E];
 
+
 % Assemble parameters
-dx = 1/Nx;
+dx = (xpts(end)-xpts(1))/Nx;
 dy = 1/Ny;
 
-params.N = Ny;
+params.Ny = Ny;
 params.Nx = Nx;
 params.dx = dx;
 params.dy = dy;
@@ -50,47 +73,57 @@ params.gamma = gamma;
 
 % SFV - with reconstructed states
 options = odeset('NonNegative', numel(ics), 'RelTol',1e-6,'AbsTol',1e-8);
-tic
+% tic
 [~,U_state] = ode45(@(t,U) rhs_2D_euler_state(t,U,params), tspan, ics, options);
-toc
+% toc
 
 sol_state = reshape(U_state(end,:),3*Nx,Ny);
+sol_state_rho = sol_state(1:Nx,:);
 
 % f1 = @() ode45(@(t,U) rhs_2D_euler_state(t,U,params), tspan, ics, options);
-% timeit(f1)
+% state_time_list = [state_time_list;timeit(f1)];
+% state_step_list = [state_step_list;size(U_state,1)];
 
 % SFV - with reconstructed fluxes
-tic
+% tic
 [~,U_flux] = ode45(@(t,U) rhs_2D_euler_flux(t,U,params), tspan, ics, options);
-toc
+% toc
 
 % f2 = @() ode45(@(t,U) rhs_2D_euler_flux(t,U,params), tspan, ics, options);
-% timeit(f2)
+% flux_time_list = [flux_time_list;timeit(f2)];
+% flux_step_list = [flux_step_list;size(U_flux,1)];
 
 sol_flux = reshape(U_flux(end,:),3*Nx,Ny);
 sol_flux_rho = sol_flux(1:Nx,:);
 sol_flux_rhou = sol_flux(Nx+1:2*Nx,:);
 sol_flux_u = sol_flux_rhou ./ sol_flux_rho;
 
-
 % Compute relative difference
 er_flux = sum(abs(sol_state - sol_flux) * (dx*dy),"all") ...
     / sum(abs(sol_state) * (dx*dy),"all");
 
+% difference_list = [difference_list;er_flux];
 disp("The relative L1 difference (recosntructed states and reconstructed fluxes) is " + er_flux)
+% 
+% end
 
-
+figure
+surf(X,Y,sol_state_rho)
+xlabel('$x$'), ylabel('$y$'), zlabel('$\rho$')
+% zlim([-0.15 1.45])
+view(52.5, 30)
+% 
 % Solution plots (with reconstructed fluxes)
 figure
 surf(X,Y,sol_flux_rho)
 xlabel('$x$'), ylabel('$y$'), zlabel('$\rho$')
-zlim([-0.15 1.45])
+% zlim([-0.15 1.45])
 view(52.5, 30)
 
 figure
 surf(X,Y,sol_flux_u)
 xlabel('$x$'), ylabel('$y$'), zlabel('$u$')
-zlim([-0.15 1.45])
+% zlim([-0.15 1.45])
 view(52.5, 30)
 
 % Compute and plot mean and std of solution (with reconstructed fluxes)
@@ -126,28 +159,50 @@ legend([h1, h2], {'Mean', 'Mean $\pm$ Std'}, 'Interpreter', 'latex',   'Location
 %% ROM - POD
 % 1D sampling
 tspan_sample = linspace(t0, tmax, 100);
-quadL = y + 0.5*(-1/sqrt(3)) * dy;
-quadR = y + 0.5*(1/sqrt(3)) * dy;  
 
-F_sample = zeros(2*Ny, 2*3*Nx*length(tspan_sample));
-quad_combined = reshape([quadL; quadR], [], 1)';
-for i = 1:length(quad_combined)
-    u = zeros(Nx,1);
-    rho = ones(Nx,1) * 0.125;
-    p = ones(Nx,1) * 0.1;
-    rho(x < 0.475+0.05*quad_combined(i)) = 1; 
-    p(x < 0.475+0.05*quad_combined(i)) = 1;     
-    E = p / (gamma - 1) + 0.5 * rho .* u.^2;    
+F_sample = zeros(Ny, 2*3*Nx*length(tspan_sample));
+for i = 1:length(y)
+    % Sampling IC for Sod shock tube
+    % u = zeros(Nx,1);
+    % rho = ones(Nx,1) * 0.125;
+    % p = ones(Nx,1) * 0.1;
+    % rho(x < 0.475+ 0.05*y(i)) = 1; 
+    % p(x < 0.475+ 0.05*y(i)) = 1;     
+    % 
+    % Sampling IC for Shu-Osher shock tube
+    u = ones(Nx,1) * 2.629369;
+    rho = ones(Nx,1) * 3.857143;
+    p = ones(Nx,1) * 10.3333;
+    u(x >= -4.1 + 0.2*y(i)) = 0;
+    rho(x >= -4.1 + 0.2 * y(i)) = 1 + 0.2 * sin(5 * x(x >= -4.1 + 0.2 * y(i)));
+    p(x >= -4.1 + 0.2*y(i)) = 1;   
+    E = p / (gamma - 1) + 0.5 * rho .* u.^2;
     ics_sample = [rho; rho.*u; E];
     F_sample(i,:)= sampling_euler(Nx, gamma, ics_sample, tspan_sample);
 end
+  a1 = 0.5*(-1/sqrt(3)); 
+  a2 = 0.5*(1/sqrt(3));
+  Fq1 = WENO_2Darray(F_sample,a1,"outflow");
+  Fq2 = WENO_2Darray(F_sample,a2,"outflow");
+  F_sample_q = zeros(2*Ny, size(F_sample,2));
 
-% Construct reduced basis
-Nmode = 40;
-[Vn,s,~] = svd(F_sample,"econ"); % ECON-SVD for large matrix
-Vn = Vn(:,1:Nmode);
+F_sample_q(1:2:end, :) = Fq1; 
+F_sample_q(2:2:end, :) = Fq2; 
 
-% Integral of basis
+% SVD 
+[Un,s,~] = svd(F_sample_q,"econ");
+% 
+% ROM_er_list = [];
+% ROM_time_list = [];
+% ROM_step_list = [];
+% N_list = 6:11;
+
+% for m = 1:length(N_list)
+% Nmode = N_list(m);
+Nmode = 6;
+Vn = Un(:,1:Nmode);
+
+% % Integral of basis
 Bn = zeros(Ny,Nmode);
 for i = 1:Ny
     Bn(i,:) = (Vn(2*i-1,:) + Vn(2*i,:)) /2;
@@ -156,20 +211,34 @@ end
 % Q-DEIM hyper-reduction
 [~,~,P] = qr(Vn',"vector");
 
-NHR = 2 * Ny;
+NHR = Nmode;
 ids = P(1:NHR);
+
+% ids = 1:2*Ny;
 
 Vn_HR = Vn(ids,:);
 
-%
+% HR indexing
+[y_ids,q1_ids,q2_ids,type_merged,perm] = HR_ids_1s(ids,Ny);
+
+
 params.Vinv = pinv(Vn_HR);
 params.B = Bn;
 params.ids = ids;
+params.y_ids = y_ids;
+params.q1_ids = q1_ids;
+params.q2_ids = q2_ids;
+params.type_merged = type_merged;
+params.perm = perm;
 
-tic
-[~,U_ROM] = ode45(@(t,U) rhs_2D_euler_interp(t,U,params), tspan, ics, options);
-toc
-%
+% tic
+[~,U_ROM] = ode45(@(t,U) rhs_2D_euler_interp_HR(t,U,params), tspan, ics, options);
+% toc
+
+% 
+% f3 = @() ode45(@(t,U) rhs_2D_euler_interp_HR(t,U,params), tspan, ics, options);
+% ROM_time_list = [ROM_time_list; timeit(f3)];
+% ROM_step_list = [ROM_step_list; size(U_ROM,1)];
 
 sol_ROM = reshape(U_ROM(end,:),3*Nx,Ny);
 sol_ROM_rho = sol_ROM(1:Nx,:);
@@ -179,20 +248,51 @@ sol_ROM_u = sol_ROM_rhou ./ sol_ROM_rho;
 % Compute ROM relative error
 er_ROM = sum(abs(sol_flux - sol_ROM) * (dx*dy),"all") ...
     / sum(abs(sol_flux) * (dx*dy),"all");
+% 
+% ROM_er_list = [ROM_er_list; er_ROM];
+% end
+
 
 disp("The relative L1 error (reconstructed fluxes and ROM) is " + er_ROM)
+
+
+% Singular value plot
+figure
+semilogy(diag(s), 'o-')
+xlabel('Mode index')
+ylabel('Singular value')
+
+% ROM error and runtime plots
+figure
+scale = 1e3;
+plot(N_list, scale * ROM_time_list./ROM_step_list, 'o-')
+hold on
+flux_time_Ny32 = flux_time_list(4)/flux_step_list(4);
+plot(N_list, scale * flux_time_Ny32 * ones(length(N_list)),'r')
+xlabel('Mode index')
+ylabel('Runtime per step (ms)')
+legend('ROM','Full SFV','Location','NW')
+xlim([6 11])
+
+figure
+semilogy(N_list, ROM_er_list, 'o-')
+xlabel('Mode index')
+ylabel('Error')
+xlim([6 11])
+ylim([1e-12 1e-2])
+
 
 % ROM solution plots
 figure
 surf(X,Y,sol_ROM_rho)
 xlabel('$x$'), ylabel('$y$'), zlabel('$\rho$')
-zlim([-0.15 1.45])
+% zlim([-0.15 4.15])
 view(52.5, 30)
 
 figure
 surf(X,Y,sol_ROM_u)
 xlabel('$x$'), ylabel('$y$'), zlabel('$u$')
-zlim([-0.15 1.45])
+% zlim([-0.15 3])
 view(52.5, 30)
 
 % Compute and plot mean and std of ROM solutions
@@ -223,4 +323,5 @@ h2 = plot(x, sol_u_mean + sol_u_std, 'LineWidth', 2, 'Color', 'r', 'LineStyle', 
 plot(x, sol_u_mean - sol_u_std, 'LineWidth', 2, 'Color', 'r', 'LineStyle', '--');
 xlabel('$x$', 'Interpreter', 'latex');
 ylabel('$u$', 'Interpreter', 'latex');
-legend([h1, h2], {'Mean', 'Mean $\pm$ Std'}, 'Interpreter', 'latex',   'Location', 'NW');
+legend([h1, h2], {'Mean', 'Mean $\pm$ Std'}, 'Interpreter', 'latex',   'Location', 'best');
+% 
